b912f26544a1b59aeab379ece2be5dbae5aead38
Erin Grisham <erinmgrisham@gmail.com>
2021-11-21 14:00:22 -0500

    test added for search bar rendering

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import App from './App';
+import { unmountComponentAtNode } from "react-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container: Element | DocumentFragment | null = null;
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container as Element);
+  (container as Element).remove();
+  container = null;
+});
+
+it('renders search bars', () => {
+  act(() => {
+    render(<App />);
+  })
+  const nameSearchBar = screen.getByPlaceholderText(/Search by name/);
+  expect(nameSearchBar).toBeInTheDocument();
+
+  const tagSearchBar = screen.getByPlaceholderText(/Search by tag/);
+  expect(tagSearchBar).toBeInTheDocument();
 });
